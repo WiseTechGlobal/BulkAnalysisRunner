cc5f75d7e463c8ff9b677977a4d3e443b7464fb6
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2017-06-28 14:34:23 +0000

    Migrate to new paket & project file format.

--- src/WTG.BulkAnalysis.Core/Properties/AssemblyInfo.cs
+++ src/WTG.BulkAnalysis.Core/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("WTG.BulkAnalysis.Core")]
 [assembly: Guid("ef81c8b4-a10d-4606-801e-d3dd02e1bfbe")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
