5c0a3e5ddfde58d54a8359c222275aa025a0c1b2
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2018-08-21 07:01:40 +0000

    Immutable command line args

--- src/WTG.BulkAnalysis.Runner/CommandLineArgs.cs
+++ src/WTG.BulkAnalysis.Runner/CommandLineArgs.cs
@@ -10,31 +10,53 @@ namespace WTG.BulkAnalysis.Runner
 	[SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses")]
 	sealed class CommandLineArgs
 	{
+		public CommandLineArgs(
+			string path,
+			IEnumerable<string> ruleIDs,
+			string filter,
+			bool fix,
+			string report,
+			string serverUrl,
+			string loadDir,
+			IEnumerable<string> loadList,
+			bool pause)
+		{
+			Path = path;
+			RuleIDs = ruleIDs;
+			Filter = filter;
+			Fix = fix;
+			Report = report;
+			ServerUrl = serverUrl;
+			LoadDir = loadDir;
+			LoadList = loadList;
+			Pause = pause;
+		}
+
 		[Value(0, Required = true, Hidden = true)]
-		public string Path { get; set; }
+		public string Path { get; }
 
 		[Value(1, Required = true, Hidden = true)]
-		public IEnumerable<string> RuleIDs { get; set; }
+		public IEnumerable<string> RuleIDs { get; }
 
 		[Option("filter", Required = false, HelpText = "When specified, only solutions matching this regular expression will be processed.")]
-		public string Filter { get; set; }
+		public string Filter { get; }
 
 		[Option("fix", Required = false, HelpText = "Attempt to apply code fixes.")]
-		public bool Fix { get; set; }
+		public bool Fix { get; }
 
 		[Option("report", Required = false, HelpText = "Specifies a file where detected issues should be recorded.")]
-		public string Report { get; set; }
+		public string Report { get; }
 
 		[Option("tfs", Required = false, Default = "http://tfs.wtg.zone:8080/tfs/CargoWise", HelpText = "Specifies the tfs project collection uri.")]
-		public string ServerUrl { get; set; }
+		public string ServerUrl { get; }
 
 		[Option("loadDir", Required = false, HelpText = "Specifies a directory where analyzer assemblies can be found.")]
-		public string LoadDir { get; set; }
+		public string LoadDir { get; }
 
 		[Option("load", Required = false, HelpText = "Specifies the assemblies to load and search for analyzers and code fixes. If none is specified, then it will attempt to use whatever the project specifies.", Separator = ';')]
-		public IEnumerable<string> LoadList { get; set; }
+		public IEnumerable<string> LoadList { get; }
 
 		[Option("pause", Required = false, Hidden = true)]
-		public bool Pause { get; set; }
+		public bool Pause { get; }
 	}
 }
